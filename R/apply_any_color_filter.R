da73d3f0c2ee61c45bbb1f67ab68ab6058a127f6
Sabina Enlund <sabina.enlund@ki.se>
2024-11-25 12:29:10 +0100

    edit text

--- R/apply_any_color_filter.R
+++ R/apply_any_color_filter.R
@@ -7,8 +7,7 @@
 #'@param image_path # the path or name (if in WD) to the image you want to filter.
 #'@param any_color = c(255, 0, 255) # Choose three RBG values (0-255) to customize the color
 #'@return a filtered image converted back to a bitmap
-#'
-#' @examples
+#'@examples
 #' # example code
 #'
 #' library(magick)
--- R/apply_grayscale_filter.R
+++ R/apply_grayscale_filter.R
@@ -8,16 +8,12 @@
 #'@return a filtered image converted back to a bitmap
 #'
 #' @examples
-#' # example code
-#'
-#' library(magick)
 #' image_path <- "Path/to/your/image_1.jpg"
 #' filtered_image <- apply_grayscale_filter(image_path)
 #' # Will generate an "filtered_image" object which can be printed and saved.
 #' print(filtered_image) # View the final image
 #' image_write(filtered_image, path = "my_image.jpg", format = "jpeg") # Save the final image as a JPEG.
-#'
-#'
+
 
 # Apply grayscale filter
 apply_grayscale_filter <- function(image_path) {
